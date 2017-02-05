60bc8bde117eb35085cf813bd37b00718ee683d4
Sttephy Blz <sttephyblz@gmail.com>
2017-02-04 17:10:13 -0700

    Acciones del controlador

--- api-restful-favoritos/app.js
+++ api-restful-favoritos/app.js
@@ -11,12 +11,6 @@ app.use(bodyParser.json());
 
 //Rutas en express
 app.use('/api', api);
-//Ruta de tipo get. Recibe ruta y una función de callback en la que se hace la funcionalidad
-//de la ruta el cual recibe la request (lo que recibe) y la respuesta (lo que devuelve)
-
-
-//Recibir parámetro por url: /prueba/:nombre, el cual lo hace obligatorio
-//En cambio si tiene un "?" al final lo pone opcional
 
 
 module.exports = app;
--- api-restful-favoritos/routes/favorito.js
+++ api-restful-favoritos/routes/favorito.js
@@ -7,5 +7,12 @@ var api = express.Router();
 //Ruta de tipo get. Recibe ruta y una función de callback en la que se hace la funcionalidad
 //de la ruta el cual recibe la request (lo que recibe) y la respuesta (lo que devuelve)
 api.get('/prueba/:nombre?', FavoritoController.prueba);
+api.get('/favorito/:id', FavoritoController.getFavorito);
+api.post('/favorito', FavoritoController.saveFavorito);
+api.put('/favorito', FavoritoController.updateFavorito);
+api.delete('/favorito/:id', FavoritoController.deleteFavorito);
+
+//Recibir parámetro por url: /prueba/:nombre, el cual lo hace obligatorio
+//En cambio si tiene un "?" al final lo pone opcional
 
 module.exports = api;
